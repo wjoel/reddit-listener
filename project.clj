7965a97c53339a77d8b1c806e20e31dd6b9a5d16
Joel Wilsson <joel.wilsson@gmail.com>
2016-02-22 23:38:19 +0100

    Add license

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject com.wjoel/reddit-listener "0.0.1-SNAPSHOT"
   :description "reddit streams"
+  :url "https://github.com/wjoel/reddit-listener"
+  :license {:name "BSD 3-clause"
+            :url "http://opensource.org/licenses/BSD-3-Clause"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  [clj-http "2.1.0"]
